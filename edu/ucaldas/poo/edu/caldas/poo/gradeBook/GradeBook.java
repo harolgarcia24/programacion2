4511118d35ffad2f1b560c05497573656dd0a53b
haroldgarcia24 <jhon.marin51541@ucaldas.edu.co>
2024-03-20 19:14:55 -0500

    Solucion del ejercicio , bug

---
+++ edu/ucaldas/poo/edu/caldas/poo/gradeBook/GradeBook.java
@@ -0,0 +1,18 @@
+package edu.caldas.poo.gradeBook;
+public class GradeBook {
+	private String courseName;
+
+	public GradeBook(String courseName){
+		this.courseName = courseName;
+	}
+	public String getCourseName(){
+		return courseName;
+	}
+	public void setCourseName(String courseName){
+		this.courseName = courseName;
+	}
+
+	public String displayMessage(){
+		return "Bienvenido a la lista de planillas : ";
+	}
+}
---
+++ edu/ucaldas/poo/edu/caldas/poo/gradeBook/GradebookTest.java
@@ -0,0 +1,9 @@
+package edu.caldas.poo.gradeBook;
+
+public class GradeBookTest {
+	public static void main(String[] args) {
+		GradeBook courseName1 = new GradeBook("numero2");
+		System.out.println(courseName1.displayMessage() + courseName1.getCourseName());
+
+	}
+}
