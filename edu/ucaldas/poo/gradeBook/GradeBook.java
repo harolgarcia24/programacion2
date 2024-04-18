58cef971747de20f464b4818f1a0c9d7156783b4
harolgarcia24 <jhon.marin51541@ucaldas.edu.co>
2024-04-17 19:22:31 -0500

    Nuevos cambios

--- edu/ucaldas/poo/gradeBook/GradeBook.java
+++ edu/ucaldas/poo/gradeBook/GradeBook.java
@@ -1,18 +1,27 @@
-package edu.ucaldas.poo.edu.ucaldas.poo.gradeBook;
+package edu.caldas.poo.gradeBook;
+
+import java.util.Scanner;
+
 public class GradeBook {
-	private String courseName;
+    private String courseName;
 
-	public GradeBook(String courseName){
-		this.courseName = courseName;
-	}
-	public String getCourseName(){
-		return courseName;
-	}
-	public void setCourseName(String courseName){
-		this.courseName = courseName;
-	}
+    public GradeBook(String courseName){
+        this.courseName = courseName;
+    }
+    public String getCourseName(){
+        return courseName;
+    }
+    public void setCourseName(String courseName){
+        this.courseName = courseName;
+    }
 
-	public String displayMessage(){
-		return "Bienvenido a la lista de planillas : ";
-	}
+    public String displayMessage(){
+        return "Bienvenido a la lista de planillas : ";
+    }
+
+    public void determineClassAverage(){
+
+    }
+
+
 }
